2c81b4104141c6bc9601be2270ea00018af893d0
Richard <rgactr@gmail.com>
2019-03-25 18:38:48 -0700

    Fixed play button displaying by default

--- src/components/Album.js
+++ src/components/Album.js
@@ -12,7 +12,7 @@ class Album extends Component {
 
     this.state = {
       album: album,
-      currentSong: album.songs[0],
+      currentSong: album.songs[-1],
       isPlaying: false,
       hovering: false,
       currentTime: 0,
